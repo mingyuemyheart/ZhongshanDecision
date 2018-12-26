188a2d625b58876c3cdd4155fd8d8896dcf6adeb
shawn <mingyuemyheart@126.com>
2018-12-26 14:17:01 +0800

    sdk 1.5.0，更新预警指南、预警图标

--- app/src/main/java/com/cxwl/shawn/zhongshan/decision/common/CONST.java
+++ app/src/main/java/com/cxwl/shawn/zhongshan/decision/common/CONST.java
@@ -1,10 +1,8 @@
 package com.cxwl.shawn.zhongshan.decision.common;
 
-import com.cxwl.shawn.zhongshan.decision.R;
-
 public class CONST {
 
-    public static final String imageSuffix = ".png";//图标后缀名//保存路径
+    public static final String imageSuffix = ".jpg";//图标后缀名//保存路径
 
     //预警颜色对应规则
     public static String[] white = {"01", "_white"};
@@ -12,11 +10,5 @@ public class CONST {
     public static String[] yellow = {"03", "_yellow"};
     public static String[] orange = {"04", "_orange"};
     public static String[] red = {"05", "_red"};
-
-    //下拉刷新progresBar四种颜色
-    public static final int color1 = R.color.refresh_color1;
-    public static final int color2 = R.color.refresh_color2;
-    public static final int color3 = R.color.refresh_color3;
-    public static final int color4 = R.color.refresh_color4;
 
 }
